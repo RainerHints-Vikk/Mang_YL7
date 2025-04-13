ff84ffcad898e1ae82042e80a852e6a25ed75251
Rainer Hints <rainer.hints@vikk.ee>
2025-04-13 14:51:55 +0300

    YL7 MÄNGu installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SelfMadePygame_YL7_RainerHintsISK24',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='None',
+    author='Rainer Hints ISK24',
+    author_email='rainer.hints@vik.ee',
+    description='Mäng mis sai loodud Progemise arvestustööks'
+)
